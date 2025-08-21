0828167acae88deeb81696cf69aab2d99a872d43
VIGNES_Ibrahim <168725336+Ibrahimpalisse@users.noreply.github.com>
2025-08-21 21:42:45 +0200

    � FIX: Corriger port et chemin du serveur PHP
    - Problème: PHP démarre mais 502 persiste
    - Port fixe: 8080 (au lieu de $PORT variable)
    - Chemin: . (au lieu de /app)
    - Index ultra-simple: Hello World
    - Suppression nginx qui peut causer conflit

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
-// Index ultra-basique pour test
-phpinfo();
+echo "Hello World from PHP!";
+echo "<br>PHP Version: " . PHP_VERSION;
+echo "<br>Server: " . $_SERVER['SERVER_SOFTWARE'] ?? 'Unknown';
+echo "<br>Date: " . date('Y-m-d H:i:s');
 ?>
